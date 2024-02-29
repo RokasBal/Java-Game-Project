6c369b899d4864f21dc1eeb4cb4beaebbb36f2f0
Rokas Baliutavičius <baliutaviciusrokas@gmail.com>
2024-02-29 16:06:22 +0200

    Cleaned up resources folder contents

--- res/dungeon 32x.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="dungeon 32x" tilewidth="32" tileheight="32" tilecount="270" columns="15">
- <image source="dungeon_set.png" width="480" height="576"/>
-</tileset>
